c89a15553b2e048805ef2c46afa6e0426d90594d
naiemhosen430 <naiemhosen430@gmail.com>
2023-12-05 20:52:27 -0800

    d

--- src/app/dashboad/page.jsx
+++ src/app/dashboad/page.jsx
@@ -6,11 +6,11 @@ export default function page() {
   const { user } = useUserAuth();
   return (
     <>
-      <div className="lg:p-10 p-0 pt-32">
+      <div className="lg:p-10 p-0 pt-32 h-screen">
         <h1 className="bg-slate-950 p-4 mb-10 lg:text-2xl text-lg rounded-lg mx-5 text-slate-500">
           Hello <span className="text-white">{user.displayName}</span>
         </h1>
-        <h1 className="bg-slate-950 p-4 lg:text-2xl text-lg rounded-lg mx-5 text-slate-500">
+        <h1 className="bg-slate-950 p-4 lg:text-2xl text-lg rounded-lg mb-5 text-slate-500">
           Order history
         </h1>
       </div>
